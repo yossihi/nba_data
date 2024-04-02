223016bb5d030afb93d0f3dd3a00e6688044227b
Yossihi <0587169@gmail.com>
2024-04-02 14:33:01 +0300

    for docker

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,5 +4,8 @@ aplication
 from django.contrib import admin
 from django.urls import path
 
+from myapp.views import all_players
+
 urlpatterns = [
+    path('all_players', all_players)
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import serializers
+from .models import Player, Team
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+
+
+class PlayerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Player
+        fields = '__all__'
+
+class TeamSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Team
+        fields = '__all__'
+
+
+@api_view(['GET'])
+def all_players(req):
+    all_players = PlayerSerializer(Player.objects.all(), many=True).data
+    return Response(all_players)
